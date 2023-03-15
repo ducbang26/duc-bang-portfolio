fbf4dea6c8fbb27c8421f60adec8d3f248632518
ducbang26 <bang26112001@gmail.com>
2023-03-15 09:48:30 +0700

    content get started section

--- constants/index.js
+++ constants/index.js
@@ -55,19 +55,19 @@ export const newFeatures = [
 export const insights = [
   {
     imgUrl: '/planet-06.png',
-    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    title: 'Nextjs TailwindCSS boilerplate',
     subtitle:
         'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
   {
     imgUrl: '/planet-07.png',
-    title: '7 tips to easily master the madness of the Metaverse',
+    title: 'Duc Bang Portfolio',
     subtitle:
         'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
   },
   {
     imgUrl: '/planet-08.png',
-    title: 'With one platform you can explore the whole world virtually',
+    title: 'DEV Community Clone',
     subtitle:
         'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
   },
--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -3,8 +3,7 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { startingFeatures } from '../constants';
-import { StartSteps, TitleText, TypingText } from '../components';
+import { TitleText, TypingText } from '../components';
 import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
 
 const GetStarted = () => (
@@ -20,26 +19,40 @@ const GetStarted = () => (
         variants={planetVariants('left')}
         className={`flex-1 ${styles.flexCenter}`}
       >
-        <img
-          src="/get-started.png"
-          alt="get-started"
-          className="w-[90%] h-[90%] object-contain"
-        />
+        <div className="border-[#888] border-2 rounded-[20px] overflow-hidden">
+          <div className="border-black border-[8px]">
+            <img
+              src="/experience-1.png"
+              alt="get-started"
+              className="w-full h-auto object-contain rounded-[12px]"
+            />
+          </div>
+        </div>
       </motion.div>
       <motion.div
         variants={fadeIn('left', 'tween', 0.2, 1)}
         className="flex-[0.75] flex justify-center flex-col"
       >
-        <TypingText title="| How Metaversus Works" />
-        <TitleText title={<>Get started with just a few clicks</>} />
-        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
-          {startingFeatures.map((feature, index) => (
-            <StartSteps
-              key={feature}
-              number={`${index < 10 ? '0' : ''} ${index + 1}`}
-              text={feature}
-            />
-          ))}
+        <TypingText title="| Selected Project" />
+        <TitleText title={<>TTC Logistics Landing Page</>} />
+        <div className="mt-[30px] flex flex-col max-w-[370px] gap-[24px]">
+          <div className="flex flex-col">
+            <div className="grid justify-center grid-cols-4 md:grid-cols-5 gap-y-[10px] md:gap-y-[20px] mb-[20px]">
+              <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" className="item flex flex-col items-center justify-around group">
+                <img className="w-[52px] h-[52px] transition duration-300 group-hover:scale-110" src="https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@main/icons/react.svg" alt="" />
+              </a>
+              <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer" className="item flex flex-col items-center justify-around group">
+                <img className="w-[52px] h-[52px] transition duration-300 group-hover:scale-110" src="https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@main/icons/next.svg" alt="" />
+              </a>
+              <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer" className="item flex flex-col items-center justify-around group">
+                <img className="w-[52px] h-[52px] transition duration-300 group-hover:scale-110" src="https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@main/icons/tailwindcss.svg" alt="" />
+              </a>
+            </div>
+            <p className="flex-1 font-normal text-[18px] text-[#B0B0B0] leading-[32.4px] mb-[20px]">
+              A landing page made with react, typescript, tailwind, nextjs with responsive for multi-device.
+            </p>
+            <a href="https://logistics.dev.pixa.asia/" target="_blank" className="flex justify-center items-center w-[150px] h-[50px] bg-[#1876d2] hover:bg-[#2884e0] text-white rounded transition duration-300" rel="noreferrer"><p className="text-[20px]">Live Demo</p></a>
+          </div>
         </div>
       </motion.div>
     </motion.div>
